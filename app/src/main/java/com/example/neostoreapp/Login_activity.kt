50b2c94717db2a4462e85c15a837f3ef572fb892
Pratiksha <Pratiksha.mokashi@neosofttech.com>
2019-05-30 16:25:27 +0530

    commited

---
+++ app/src/main/java/com/example/neostoreapp/Login_activity.kt
@@ -0,0 +1,12 @@
+package com.example.neostoreapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}
